97cb49389a65249aad6ee9bf036f15f6954ffd51
Angel Martinez <angel.codes139@gmail.com>
2020-09-02 11:56:04 -0500

    home page

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
+// Pages
+import 'package:counter_app/src/pages/home_page.dart';
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(ctx) {
     return MaterialApp(
       home: Center(
-        child: Text('Hello World!'),
+        child: HomePage(),
       ),
     );
   }
