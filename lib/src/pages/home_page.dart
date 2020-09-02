eabfb7bfc6e59bbba0b923f50b60c5dcb427e017
Angel Martinez <angel.codes139@gmail.com>
2020-09-02 12:14:07 -0500

    column widget - styling the text

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,15 +1,29 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  final textStyles = new TextStyle(fontSize: 20.0);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Title'),
+        title: Text('Counter App'),
         centerTitle: true,
       ),
       body: Center(
-        child: Text('Hello world!'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Number of tabs:',
+              style: textStyles,
+            ),
+            Text(
+              '0',
+              style: textStyles,
+            ),
+          ],
+        ),
       ),
     );
   }
