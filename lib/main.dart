3e0bb093bef9af40895716f46b9424c6f911ec7c
Angel Martinez <angel.codes139@gmail.com>
2020-09-02 11:41:51 -0500

    creating other file

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'src/app.dart';
+
 void main() {
   runApp(new MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(ctx) {
-    return MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
-      ),
-    );
-  }
 }
---
+++ lib/src/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(ctx) {
+    return MaterialApp(
+      home: Center(
+        child: Text('Hello World!'),
+      ),
+    );
+  }
+}
