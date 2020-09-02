749f1e2148b09487539bd24062d8c0c99ede9aeb
Angel Martinez <angel.codes139@gmail.com>
2020-09-02 14:46:39 -0500

    removing debugging banner

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(ctx) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Center(
         child: CounterPage(),
       ),
